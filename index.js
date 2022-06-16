fbe001405a11ce3c455c5f20448f8cb85529ec26
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-06-16 13:29:40 -0700

    cors

--- index.js
+++ index.js
@@ -24,22 +24,25 @@ const { check, validationResult } = require('express-validator');
 
 //cors implementation
 const cors = required('cors');
-let allowedOrigins = [
-    'http://localhost:8089',
-    'http://testsite.com'
-];
+app.use(cors());
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null,true);
-        if(allowedOrigins.indexOf(origin) === -1){
+
+//let allowedOrigins = [
+   // 'http://localhost:8089',
+    // 'http://testsite.com'
+// ];
+
+// app.use(cors({
+   // origin: (origin, callback) => {
+     //   if(!origin) return callback(null,true);
+      //  if(allowedOrigins.indexOf(origin) === -1){
             //if a specific origin isn't found on the list of allowed origins
-            let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null,true);
-    }
-}));
+        //    let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+         //   return callback(new Error(message ), false);
+       // }
+       // return callback(null,true);
+   // }
+// }));
 
 
 
