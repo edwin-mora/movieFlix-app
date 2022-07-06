344ffbf252d6222c709e9b0b979d2029b045ad04
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-06 13:06:03 -0700

    cors authentication

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ const cors = require('cors');
 app.use(cors());
 
 
-//let allowedOrigins = [
-   // 'http://localhost:8089',
-    // 'http://testsite.com'
-// ];
+let allowedOrigins = [
+   'http://localhost:1234',
+    'http://testsite.com'
+];
 
-// app.use(cors({
-   // origin: (origin, callback) => {
-     //   if(!origin) return callback(null,true);
-      //  if(allowedOrigins.indexOf(origin) === -1){
+ app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null,true);
+        if(allowedOrigins.indexOf(origin) === -1){
             //if a specific origin isn't found on the list of allowed origins
-        //    let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-         //   return callback(new Error(message ), false);
-       // }
-       // return callback(null,true);
-   // }
-// }));
+        let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+       return callback(new Error(message ), false);
+     }
+    return callback(null,true);
+ }
+}));
 
 
 
