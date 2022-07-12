b3a8db74eb6c3b5b540ca5f9645c963cc17955b9
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-11 23:47:15 -0700

    idk

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.get('/movies/directors/:Name', passport.authenticate('jwt', { session: false
 app.post('/users', [check('Username', 'Username is required.').isLength({min:5}),
     check('Username', 'Username contains non alphanumeric characters - not allowed!').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail() ],  (req, response) => {
+    check('Email', 'Email does not appear to be valid').isEmail() ], passport.authenticate('jwt', { session: false }), (req, response) => {
     //check the validation object for any errors
     let errors = validationResult(req);
 
