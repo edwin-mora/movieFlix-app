39c7afe98589bd856e24a2c7a011af7c37847b7b
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-12 23:58:40 -0700

    img6

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ let auth = require('./auth.js')(app);
 const passport = require('passport');
 require('./passport.js');
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+});
+
 
 const { check, validationResult } = require('express-validator');
 
