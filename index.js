b95fe32bdac1b06597308344c6bb4fde474be475
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-13 23:00:19 -0700

    img10

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors());
     'http://testsite.com'
 ]; */
 
- app.use(cors({
+ /* app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null,true);
         if(allowedOrigins.indexOf(origin) === -1){
@@ -42,7 +42,7 @@ app.use(cors());
      }
     return callback(null,true);
  }
-})); 
+})); */
 
 
 
